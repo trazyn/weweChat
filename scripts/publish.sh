737dc6cf1f2c6ac0702cffc8ae6cdc70b2b5d3e5
trazyn <tn.razy@gmail.com>
2017-09-18 15:28:14 +0800

    test auto update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,4 +7,4 @@ if [ -z "$GH_TOKEN" ]; then
 fi
 
 # This will build, package and upload the app to GitHub.
-node_modules/.bin/build --win --mac --linux -p always
+npm run build && node_modules/.bin/build --projectDir ./dist --win --mac --linux -p always
